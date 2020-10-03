31753d55df30e047988a1ff47ae4e2c6259506c5
trs-dev <taras2017ua@gmail.com>
2020-10-03 20:50:10 +0300

    Added NonResidentialPremise and LandPlot models

---
+++ RealEstateBaseWebApi/Models/LandPlot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateBaseWebApi.Models
+{
+    public class LandPlot
+    {
+        public string Type { get; set; } //for construction, farming, ...
+
+        public float TotalSquare { get; set; }
+
+        public string Address { get; set; }
+    }
+}
---
+++ RealEstateBaseWebApi/Models/NonResidentialPremise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateBaseWebApi.Models
+{
+    public class NonResidentialPremise
+    {
+        public string Type { get; set; } //garage, office, shop, ...
+
+        public int Rooms { get; set; }
+        public int Floor { get; set; }
+
+        public float TotalSquare { get; set; }
+
+        public string MaterialOfWall { get; set; }
+        public int YearOfConstruction { get; set; }
+
+        public string Address { get; set; }
+    }
+}
