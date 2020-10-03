2ebce7f13250c447b00478edf97c0b6af37c64eb
trs-dev <taras2017ua@gmail.com>
2020-10-03 20:43:13 +0300

    Added Apartment model

---
+++ RealEstateBaseWebApi/Models/Apartment .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateBaseWebApi.Models
+{
+    public class Apartment
+    {
+        public int Rooms { get; set; }
+        public int Floor { get; set; }
+
+        public float TotalSquare { get; set; }
+        public float LiveSquare { get; set; }
+        public float KitchenSquare { get; set; }
+
+        public string MaterialOfWall { get; set; }
+        public int YearOfConstruction { get; set; }
+
+        public string Address { get; set; }
+    }
+}
