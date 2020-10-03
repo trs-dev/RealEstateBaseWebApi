50cda490a545c62a18b8e17cceb9cdc5e34b78a0
trs-dev <taras2017ua@gmail.com>
2020-10-03 21:19:14 +0300

    Added database context - RealtyContext

---
+++ RealEstateBaseWebApi/Models/RealtyContext .cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateBaseWebApi.Models
+{
+    public class RealtyContext : DbContext      //one context for all types of realty
+    {
+        public DbSet<Apartment> Apartments { get; set; }
+        public DbSet<LandPlot> LandPlots { get; set; }
+        public DbSet<NonResidentialPremise> NonResidentialPremises { get; set; }
+
+
+        public RealtyContext(DbContextOptions<RealtyContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
