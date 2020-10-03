e34c8a4e7717c2ab02dabc5e086957a361f7b633
trs-dev <taras2017ua@gmail.com>
2020-10-03 22:32:23 +0300

    Added data validation attributes into all models

--- RealEstateBaseWebApi/Models/LandPlot.cs
+++ RealEstateBaseWebApi/Models/LandPlot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,18 @@ namespace RealEstateBaseWebApi.Models
     {
         public int Id { get; set; }
 
+        [StringLength(50)]
         public string Type { get; set; } //for construction, farming, ...
 
+        [Range(0, 10000)]
         public float TotalSquare { get; set; }
 
+        [Required]
+        [StringLength(500)]
         public string Address { get; set; }
 
+        [Required]
+        [Range(0, 100000000)]
         public int Price { get; set; }
     }
 }
--- RealEstateBaseWebApi/Models/NonResidentialPremise.cs
+++ RealEstateBaseWebApi/Models/NonResidentialPremise.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,18 +10,30 @@ namespace RealEstateBaseWebApi.Models
     {
         public int Id { get; set; }
 
+        [StringLength(50)]
         public string Type { get; set; } //garage, office, shop, ...
 
+        [Range(0, 20)]
         public int Rooms { get; set; }
+
+        [Range(-20, 500)]
         public int Floor { get; set; }
 
+        [Range(0, 1000)]
         public float TotalSquare { get; set; }
 
+        [StringLength(20)]
         public string MaterialOfWall { get; set; }
+
+        [Range(1900, 2050)]
         public int YearOfConstruction { get; set; }
 
+        [Required]
+        [StringLength(500)]
         public string Address { get; set; }
 
+        [Required]
+        [Range(0, 100000000)]
         public int Price { get; set; }
     }
 }
